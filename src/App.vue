268a9f1a51c50ba6c2c96250671cfe8c971883b2
lyxlwz <54365306+lyxlwz@users.noreply.github.com>
2020-08-17 14:20:59 +0800

    v0.1

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>hhh</h2>
+        <!--
+        <router-link to="./page/API/index.vue"></router-link>
+        <router-link to="./page/cloud/index.vue"></router-link>
+        <router-link to="./page/component/index.vue"></router-link>
+        -->
+    </div>
+</template>
+<script>
+import "./onekit/OnekitPage"
+export default OnekitPage({
+    onLoad(){
+        console.log("onLoad");
+    }
+})
+</script>
