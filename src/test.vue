55c9b172cf5f16baed29024e447a17d1932653a5
lyxlwz <54365306+lyxlwz@users.noreply.github.com>
2020-08-18 17:22:45 +0800

    v0.1

--- src/test.vue
+++ src/test.vue
@@ -5,19 +5,18 @@
 import wx from "./onekit/wx"
 import OnekitPage from "./onekit/OnekitPage"
 export default OnekitPage({
-  onLoad() {
-    wx.getSystemInfo({
-    success (res) {
-        console.log("getSystemInfo",res)
-    }
-  })
-  /*
-    try {
-      const res = wx.getSystemInfoSync()
-      console.log(res.windowWidth,"xxx")
-    } catch (e) {
-      console.log("false");
-    }*/
+  onLoad: function (options) {
+    var obj = wx.getLaunchOptionsSync()
+    console.log('启动小程序的路径:',obj.path)
+    console.log('启动小程序的场景值:', obj.scene)
+    console.log('启动小程序的 query 参数:', obj.query)
+    console.log('来源信息:', obj.shareTicket)
+    console.log('来源信息参数appId:', obj.referrerInfo.appId)
+    console.log('来源信息传过来的数据:', obj.referrerInfo.extraData)
+  },
+  mounted: function (options) {
+    var obj = wx.getLaunchOptionsSync()
+    window.addEventListener("pageshow",obj)
   }
 })
 </script>
