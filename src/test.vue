2d43ea407d932f13ae6bc9c620ebd2276a0d1ba3
lyxlwz <54365306+lyxlwz@users.noreply.github.com>
2020-08-20 15:28:40 +0800

    v0.1

--- src/test.vue
+++ src/test.vue
@@ -1,24 +1,29 @@
 <template>
-  <div>Test</div>
+  <div class="container">kkk</div>
 </template>
 <script>
 import wx from "./onekit/wx";
 import OnekitPage from "./onekit/OnekitPage";
 
+import Storage from "./onekit/api/Storage/Storage";
+
 export default OnekitPage({
   onLoad: function () {
-    wx.downloadFile({
-      url: 'https://www.baidu.com/img/flexible/logo/pc/result@2.png', //仅为示例，并非真实的资源
-      success (res) {
-        // 只要服务器有响应数据，就会把响应内容写入文件并进入 success 回调，业务需要自行判断是否下载到了想要的内容
-        /*if (res.statusCode === 200) {
-          wx.playVoice({
-            filePath: res.tempFilePath
-          }) 
-        }*/
-        console.log(res);
-      }
-    })
-  }
+    //  wx.setStorage({
+    //       key:"2",
+    //       data:"3",
+    //       success(){
+    //            wx.clearStorage()
+    //       }
+    //  });
+      wx.clearStorageSync()
+    console.log( wx.clearStorageSync(),"xxx")
+  },
 });
 </script>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
