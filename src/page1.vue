724c9c18d4c6963085724ff58287b85ae72fe143
WangYeWei <826036140@qq.com>
2020-11-05 12:10:14 +0800

    .

--- src/page1.vue
+++ src/page1.vue
@@ -1,41 +1,24 @@
 <template>
   <page id="app">
-    <onekit-view onekit-class="section section_gap">
-      <onekit-label>
-        <onekit-view onekit-class="section__title">type="switch"</onekit-view>
-
-        <onekit-switch checked @change="switch1Change"></onekit-switch>
-
-      </onekit-label>
-    </onekit-view>
+    <onekit-view @tap="test_click">click</onekit-view>
 
   </page>
 </template>
 
 <script>
-  import OnekitPage from '../weixin2vue/OnekitPage';
+  import { OnekitPage } from '../weixin2vue/OnekitPage';
+  import { wx } from '../weixin2vue/wx';
 
-  var pageData = {
-    data: {
-      switch1Checked: true,
-      switch2Checked: false,
-      switch1Style: '',
-      switch2Style: 'text-decoration: line-through'
+  export default OnekitPage({
+    data: {},
+    test_click: function () {
+      wx.redirectTo({
+        url: './page2.vue',
+        success: (res) => { console.log(res) },
+        fail: console.log
+      });
     }
-  };
-  for (var i = 1;
-    (i <= 2); ++i) {
-    (function (index) {
-      pageData[`switch${index}Change`] = function (e) {
-        console.log(`switch${index}发生change事件，携带值为`, e);
-        var obj = {};
-        this.setData(obj);
-        obj = {};
-        this.setData(obj);
-      };
-    })(i);
-  }
-  export default OnekitPage({}, pageData);
+  });
 </script>
 
 <style>
