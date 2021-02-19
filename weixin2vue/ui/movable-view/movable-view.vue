c414438746a75d680c76567a94d333f3425b5a74
WangYeWei <826036140@qq.com>
2021-02-19 15:25:37 +0800

    .

--- weixin2vue/ui/movable-view/movable-view.vue
+++ weixin2vue/ui/movable-view/movable-view.vue
@@ -3,8 +3,7 @@
        :class="['onekit-movable-view',onekitClass]"
        :style="onekitStyle"
        :id="onekitId"
-       class="onekit-movable-view-own"
-       @touchmove="moving">
+       class="onekit-movable-view-own">
     <slot></slot>
   </div>
 </template>
@@ -15,20 +14,77 @@
   export default {
     name: "onekit-movable-view",
     mixins: [weixin_behavior, onekit_behavior],
+    data() {
+      return {
+        defaults: {
+          edge: true
+        },
+        params: {},
+        options: {},
+        distanceX: 0,
+        distanceY: 0,
+        win: window,
+        winWith: 0,
+        winHeight: 0
+        /*options || {}*/
+      }
+    },
     props: {
-
+      'direction': {
+        type: String,
+        default: 'none'
+      },
+      'intertia': Boolean,
+      'out-of-bounds': Boolean,
+      'x': Number,
+      'y': Number,
+      'damping': {
+        type: Number,
+        default: 20
+      },
+      'friction': {
+        type: Number,
+        default: 2
+      },
+      'disabled': Boolean,
+      'scale': Boolean,
+      'scale-min': {
+        type: Number,
+        default: 0.5
+      },
+      'scale-max': {
+        type: Number,
+        default: 10
+      },
+      'scale-value': {
+        type: Number,
+        default: 1
+      },
+      'animation': {
+        type: Boolean,
+        default: true
+      },
 
     },
-    methods: {
-      moving() {
-        console.log('ok')
+    methods: {},
+    created() {
+
+      this.winWith = window.innerWidth
+      this.winHeight = window.innerHeight
+
+      for (const key in this.defaults) {
+        if (typeof this.options[key] !== 'undefined') {
+          this.params[key] = this.options[key]
+        } else {
+          this.params[key] = this.defaults[key]
+        }
       }
     }
   }
 </script>
 
 <style>
-  .onekit-movable-view-own {
+  /* .onekit-movable-view-own {
     position: absolute;
-  }
+  } */
 </style>
