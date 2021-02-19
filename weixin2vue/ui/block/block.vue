e8655467233119bd1473b900415fd7b8e473c742
15802303637 <>
2021-02-19 15:45:55 +0800

    .

---
+++ weixin2vue/ui/block/block.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="onekit-page">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "onekit-block",
+};
+</script>
+
+<style scoped>
+</style>
---
+++ weixin2vue/ui/block/index.js
@@ -0,0 +1,3 @@
+import page from './page.vue'
+page.install = (Vue) => Vue.component(page.name, page);
+export default page
