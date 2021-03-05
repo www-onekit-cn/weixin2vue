ac6e313b59f0b5586d92f69da1c885ed6e665c65
WangYeWei <826036140@qq.com>
2021-03-05 18:18:33 +0800

    .

--- weixin2vue/ui/live-pusher/live-pusher.vue
+++ weixin2vue/ui/live-pusher/live-pusher.vue
@@ -1,27 +1,34 @@
 <template>
-		<div
-		:class="['onekit-live-pusher',onekitClass]"
-		:style="onekitStyle"
-		:id="onekitId"
-		>
-		<slot></slot>
-		</div>
+	<video :class="['onekit-live-pusher',onekitClass]" :style="onekitStyle" :id="onekitId">
+
+	</video>
 </template>
 
 <script>
-import weixin_behavior from "../../behaviors/weixin_behavior"
-import onekit_behavior from "../../behaviors/onekit_behavior"
-export default{
-    name: "onekit-live-pusher",
-    mixins:[weixin_behavior, onekit_behavior],
-		props:{
-		
-			
+	import weixin_behavior from "../../behaviors/weixin_behavior"
+	import onekit_behavior from "../../behaviors/onekit_behavior"
+	import wx from '../../wx'
+	// import Vue from 'vue'
+	// const wx = new WX(Vue.prototype)
+	export default {
+		name: "onekit-live-pusher",
+		mixins: [weixin_behavior, onekit_behavior],
+		props: {
+
+
 		},
-		methods:{
-			
+		methods: {
+
+		},
+		mounted() {
+			wx.chooseVideo({
+				sourceType: 'camera',
+				success: res => {
+					console.log(res)
+				}
+			})
+		}
 	}
-}
 </script>
 
 <style>
